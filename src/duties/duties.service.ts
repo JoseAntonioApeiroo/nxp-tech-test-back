29b1ddd01c35b3e5eed107e4b22939cd85b9bc82
Jose Antonio Nieto <jn@apeiroo.com>
2025-11-07 11:50:00 +0100

    feat: data validation

--- src/duties/duties.service.ts
+++ src/duties/duties.service.ts
@@ -3,13 +3,22 @@ import { InsertDuty } from "./dtos/insertDuty.dto";
 import { Duty } from "./models/duty.model";
 
 export async function insertDuty(duty: InsertDuty): Promise<Duty> {
+
+    if (typeof duty?.name != 'string' || duty.name.length == 0) {
+        throw new Error('Value name is required on the body');
+    }
+
     try {
         const response = await database.query(
             'INSERT INTO duty (id, name) VALUES ($1, $2) RETURNING id, name',
             [crypto.randomUUID(), duty.name]
         );
 
-        return response.rows[0];
+        if (response.rows.length == 0) {
+            throw new Error('Wrong query response data');
+        } else {
+            return response.rows[0];
+        }
     } catch (error) {
         throw new Error('Error inserting data');
     }
@@ -28,13 +37,24 @@ export async function getDuties(): Promise<Duty[]> {
 }
 
 export async function updateDuty(duty: Duty) {
+
+    if (typeof duty?.id != 'string' || duty.id.length != 36) {
+        throw new Error('Value id is required on the body');
+    } else if (typeof duty?.name != 'string' || duty.name.length == 0) {
+        throw new Error('Value name is required on the body');
+    }
+
     try {
         const response = await database.query(
             'UPDATE duty SET name = $1 WHERE id = $2 RETURNING id, name',
             [duty.name, duty.id]
         );
 
-        return response.rows[0];
+        if (response.rows.length == 0) {
+            throw new Error('Wrong query response data');
+        } else {
+            return response.rows[0];
+        }
     } catch (error) {
         throw new Error('Error updating duty');
     }
--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: '*', // permite todos los dominios
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
